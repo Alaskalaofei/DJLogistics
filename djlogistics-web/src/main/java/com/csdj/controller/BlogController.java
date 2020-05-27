14383b9ad88cdd983d5f72d97da4e1b6fe146af0
fyq <1524503229@qq.com>
2020-05-27 17:20:34 +0800

    添加页面

--- djlogistics-web/src/main/java/com/csdj/controller/BlogController.java
+++ djlogistics-web/src/main/java/com/csdj/controller/BlogController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/aa")
 public class BlogController {
 
     @Autowired
---
+++ djlogistics-web/src/main/java/com/csdj/controller/ToPage.java
@@ -0,0 +1,13 @@
+package com.csdj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ToPage {
+    @RequestMapping("/{page}")
+    public String page(@PathVariable(value = "page") String page){
+        return  page;
+    }
+}
