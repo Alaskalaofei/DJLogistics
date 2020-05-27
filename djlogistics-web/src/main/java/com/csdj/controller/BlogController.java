fa456b00348da902ec8e9a2b4252bfd4f103e4f3
Alaskalaofei <3070649228@qq.com>
2020-05-27 15:30:45 +0800

    lx测试html网页数据

--- djlogistics-web/src/main/java/com/csdj/controller/BlogController.java
+++ djlogistics-web/src/main/java/com/csdj/controller/BlogController.java
@@ -3,20 +3,34 @@ package com.csdj.controller;
 import com.csdj.pojo.blog;
 import com.csdj.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class BlogController {
 
     @Autowired
     private BlogService blogService;
 
     @RequestMapping("/list")
+    @ResponseBody
     public List<blog> list()
     {
         return blogService.blogList();
     }
+
+    @RequestMapping("/blog")
+    public String blog(Model model)
+    {
+        List<blog> blogList=blogService.blogList();
+        model.addAttribute("blog",blogList);
+        return "list";
+    }
+
+
 }
